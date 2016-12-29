46ec57da754d5fd39ced9fa80cc299063b4adb0d
Taha Husain <taha.husain@weboniselab.com>
2016-12-29 15:03:28 +0530

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-postgis "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [korma               "0.4.3"]])
---
+++ test/clj_postgis/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-postgis.core-test
+  (:require [clojure.test :refer :all]
+            [clj-postgis.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
